174c17cb10d1e524a449262fd57cfc66946dc934
aedades <amanda@glympse.com>
2018-04-27 14:45:44 -0700

    Use standard single quotes

--- src/consul.py
+++ src/consul.py
@@ -7,7 +7,7 @@ logger = logging.getLogger('consul')
 
 class Consul:
     def __init__(self, address, port, secure, token):
-        self._address = f"https://{address}:{port}"if secure is True else f"http://{address}:{port}"
+        self._address = f'https://{address}:{port}'if secure is True else f'http://{address}:{port}'
         self._token = token
 
     def backup_kv(self):
